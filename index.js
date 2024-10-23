a50e56fd5cf864c06545b13be4fd1c9027d40133
Walter Moar <Walter.Moar@gov.bc.ca>
2024-10-23 16:31:43 -0700

    fix: remove unused root route to check codeql

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (_req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/", (_req, res) => {
+//   res.send("Hello World!");
+// });
 
 const router = express.Router();
 const middleware = require("./middleware")
